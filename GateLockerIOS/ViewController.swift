8cbaa4391f1e55df26571b86eb0cac68def8a8d9
BHD233 <32787292+BHD233@users.noreply.github.com>
2020-02-06 21:07:23 +0700

    Initial Commit

---
+++ GateLockerIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GateLockerIOS
+//
+//  Created by Bùi Đức on 2/6/20.
+//  Copyright © 2020 bhd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
